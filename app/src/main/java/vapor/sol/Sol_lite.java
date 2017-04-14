fba7b79edbbdb796eda8053e8a639caa417219a3
GoogleCloudPlatform101 <ngx874@mocs.utc.edu>
2017-04-14 00:59:28 -0400

    Update Sol_lite.java

--- app/src/main/java/vapor/sol/Sol_lite.java
+++ app/src/main/java/vapor/sol/Sol_lite.java
@@ -1,5 +1,6 @@
 package vapor.sol;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,6 +24,7 @@ public class Sol_lite extends AppCompatActivity {
     EditText commandInput;
     TextView title;
     TextView gameOutput;
+    //TextView drawline;
     //suit piles
     //hearts
     TextView line01;
@@ -49,6 +51,9 @@ public class Sol_lite extends AppCompatActivity {
     String five;
     String six;
     String seven;
+    String greeting;
+    String input;
+    //String dline;
 
 
     @Override
@@ -57,7 +62,11 @@ public class Sol_lite extends AppCompatActivity {
         setContentView(R.layout.activity_sol_lite);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        game = new Sol_lite();
 
+        /*
+         * Handles the input and input enter button
+         */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,12 +74,58 @@ public class Sol_lite extends AppCompatActivity {
                 commandInput = (EditText) findViewById(R.id.commandInput);
                 gameOutput = (TextView) findViewById(R.id.gameOutput);
                 gameOutput.setText(commandInput.getText());
+                input = commandInput.getText().toString();
+                game.gameCommands(input);
+
+                //maybe this will update it every time
+                one = "one " + game.p.one.pile.toString();
+                two = "two " + game.p.two.pile.toString();
+                three = "three " + game.p.three.pile.toString();
+                four = "four " + game.p.four.pile.toString();
+                five = "five " + game.p.five.pile.toString();
+                six = "six " + game.p.six.pile.toString();
+                seven = "seven " + game.p.seven.pile.toString();
+
+                //dline = "draw " + game.p.draw.q.peek().toString();
+                //drawline.setText(dline);
+
+                if(game.p.hearts.pile.empty()){
+                    line01.setText("♥ ");
+                }
+                else{
+                    line01.setText(game.p.hearts.pile.peek().toString());
+                }
+                if(game.p.spades.pile.empty()){
+                    line02.setText("♠ ");
+                }
+                else{
+                    line02.setText(game.p.spades.pile.peek().toString());
+                }
+                if(game.p.diamonds.pile.empty()){
+                    line03.setText("♦ ");
+                }
+                else{
+                    line03.setText(game.p.diamonds.pile.peek().toString());
+                }
+                if(game.p.clubs.pile.empty()){
+                    line04.setText("♣ ");
+                }
+                else{
+                    line04.setText(game.p.clubs.pile.peek().toString());
+                }
+                line05.setText(one);
+                line06.setText(two);
+                line07.setText(three);
+                line08.setText(four);
+                line09.setText(five);
+                line10.setText(six);
+                line11.setText(seven);
+
                 commandInput.setText(null);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        game = new Sol_lite();
         //creates strings for playpiles
         one = "one " + game.p.one.pile.toString();
         two = "two " + game.p.two.pile.toString();
@@ -79,8 +134,15 @@ public class Sol_lite extends AppCompatActivity {
         five = "five " + game.p.five.pile.toString();
         six = "six " + game.p.six.pile.toString();
         seven = "seven " + game.p.seven.pile.toString();
+        //dline = "draw " + game.p.draw.q.peek().toString();
 
-        title.setText(game.greetingMessage());
+        title = (TextView) findViewById(R.id.title);
+        greeting = game.greetingMessage();
+        title.setText(greeting);
+
+        //drawline = (TextView) findViewById(R.id.drawline);
+        //drawline.setText(dline);
+
 
         line01 = (TextView) findViewById(R.id.line01);
         //if the pile is empty show the symbol, else the top card to string. true for all suitpiles
@@ -125,7 +187,6 @@ public class Sol_lite extends AppCompatActivity {
         line10.setText(six);
         line11 = (TextView) findViewById(R.id.line11);
         line11.setText(seven);
-
     }
 
 
@@ -543,13 +604,9 @@ public class Sol_lite extends AppCompatActivity {
                     return true;
                 }
             }
-        /*
-         * these handle moving from suit piles
-         * CURRENTLY BREAKS CODE NEED OF REPAIR
-         * (non vital b/c unlikely move)
+            /*
             else if(c1.equalsIgnoreCase("spades")){
                 if(c2.equalsIgnoreCase("seven")){
-                    //p.spades.addCard(p.seven);
                     p.spades.addCard(p.seven);
                     return true;
                 }
@@ -1264,7 +1321,7 @@ class SuitPiles{
     * @param p  target play pile
     */
 
-    public void add(PlayPiles p){
+    public void addCard(PlayPiles p){
         Cards c = p.pile.peek();
         Cards val;
         //checks if the suit is correct
