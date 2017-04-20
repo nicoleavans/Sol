d2f4134df6e3ba15f295949a9785e3442edf8a3d
nicoleavans <cnicoleavans@live.com>
2017-04-19 22:03:03 -0400

    updates to sol_lite
    *fixed oops

--- app/src/main/java/vapor/sol/Sol_lite.java
+++ app/src/main/java/vapor/sol/Sol_lite.java
@@ -100,12 +100,17 @@ public class Sol_lite extends AppCompatActivity {
                 line10.setText(six);
                 line11.setText(seven);
 
-                if(game.p.clubs.pile.peek().getValue() == 13 &&
-                        game.p.hearts.pile.peek().getValue() == 13 &&
-                        game.p.diamonds.pile.peek().getValue() == 13 &&
-                        game.p.spades.pile.peek().getValue() == 13){
-                    Toast toast = Toast.makeText(getApplicationContext(), "You win!", Toast.LENGTH_LONG);
-                    toast.show();
+                if(!game.p.clubs.pile.empty() &&
+                        !game.p.diamonds.pile.empty() &&
+                        !game.p.spades.pile.empty() &&
+                        !game.p.hearts.pile.empty()){
+                    if(game.p.clubs.pile.peek().getValue() == 13 &&
+                            game.p.hearts.pile.peek().getValue() == 13 &&
+                            game.p.diamonds.pile.peek().getValue() == 13 &&
+                            game.p.spades.pile.peek().getValue() == 13){
+                        Toast toast = Toast.makeText(getApplicationContext(), "You win!", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
                 }
                 commandInput.setText(null);
             }
